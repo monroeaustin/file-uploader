f5c0113fa838212adf352a5f45f963b9da4194a8
Monroe Austin <monroeraustin2@yahoo.com>
2025-08-15 00:59:11 -0700

    Feat: Header EJS View

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('./home')
 })
 
 app.listen(port, () => {
